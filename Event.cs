9d23b91cb9a0b5ac6d3e62b1b9b2b2b84927678c
Дмитриев <dmitrievd125@gmail.com>
2026-04-09 13:13:50 +0300

    Дмитриев - добавлен класс Event

---
+++ Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager
+{
+    public class Event
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+        public Event(DateTime date, string description)
+        {
+            Date = date;
+            Description = description;
+        }
+    }
+}
