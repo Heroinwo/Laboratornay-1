16222dd243db62dfcf249a6caaecd5246efb8706
Дмитриев <dmitrievd125@gmail.com>
2026-04-09 13:20:01 +0300

    Дмитриев - Добавлен класс Form1(основная форма)

---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace ProductManager {
+    public class CalendarForm : Form
+    {
+        private CalendarManager calendarManager;
+        private DateTimePicker datePicker;
+        private TextBox descriptionTextBox;
+        private Button addEventButton;
+        private ListBox eventsListBox;
+        private Button removeEventButton;
+
+        public CalendarForm()
+        {
+            this.Text = "Календарь событий";
+            this.Width = 400;
+            this.Height = 400;
+
+            datePicker = new DateTimePicker
+            {
+                Location = new System.Drawing.Point(10, 10)
+            };
+
+            descriptionTextBox = new TextBox
+            {
+                Location = new System.Drawing.Point(10, 40),
+                Width = 200
+            };
+
+            addEventButton = new Button
+            {
+                Location = new System.Drawing.Point(10, 70),
+                Text = "Добавить",
+                Width = 100
+            };
+            addEventButton.Click += AddEventButton_Click;
+
+            eventsListBox = new ListBox
+            {
+                Location = new System.Drawing.Point(10, 100),
+                Width = 200,
+                Height = 200
+            };
+
+            removeEventButton = new Button
+            {
+                Location = new System.Drawing.Point(220, 100),
+                Text = "Удалить",
+                Width = 80,
+                Height = 20
+            };
+            removeEventButton.Click += RemoveEventButton_Click;
+
+            this.Controls.Add(datePicker);
+            this.Controls.Add(descriptionTextBox);
+            this.Controls.Add(addEventButton);
+            this.Controls.Add(eventsListBox);
+            this.Controls.Add(removeEventButton);
+
+            calendarManager = new CalendarManager();
+            UpdateEventsList();
+        }
+
+        private void UpdateEventsList()
+        {
+            eventsListBox.Items.Clear();
+            foreach (var ev in calendarManager.Events)
+            {
+                eventsListBox.Items.Add($"{ev.Date.ToString("yyyy-MM-dd")} - {ev.Description}");
+            }
+        }
+
+        public void AddEventButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(descriptionTextBox.Text))
+            {
+                MessageBox.Show("Введите описание события!");
+                return;
+            }
+            Event newEvent = new Event(datePicker.Value, descriptionTextBox.Text);
+            try
+            {
+                calendarManager.AddEvent(newEvent);
+                descriptionTextBox.Clear();
+                UpdateEventsList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void RemoveEventButton_Click(object sender, EventArgs e)
+        {
+            if (eventsListBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите событие для удаления!");
+                return;
+            }
+            string selectedItem = eventsListBox.SelectedItem.ToString();
+            DateTime date;
+            if (DateTime.TryParse(selectedItem.Split(new[] { '-' }, StringSplitOptions.None)[0], out
+    date))
+            {
+                var eventToRemove = calendarManager.Events.Find(ev => ev.Date == date);
+                if (eventToRemove != null)
+                {
+                    try
+                    {
+                        calendarManager.RemoveEvent(eventToRemove);
+                        UpdateEventsList();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+    
+}
